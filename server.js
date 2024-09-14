5679a028ccac4cad350983ed04fe73fc86dad615
jgranger3 <jgranger3@ivytech.edu>
2024-09-14 07:20:27 -0400

    sending simple html

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const http = require('http');
 const server = http.createServer((req, res) =>{
     console.log(req.url, req.method);
     //set header content type
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
 
-    res.write('hello, ninjas');
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>hello, ninjas</p>');
+    res.write('<p>hello again, ninjas</p>');
     res.end();
 
 });
