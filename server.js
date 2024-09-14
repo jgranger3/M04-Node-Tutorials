27d1e3f5026b68cdefcdcfa3e3f2e2258139251b
jgranger3 <jgranger3@ivytech.edu>
2024-09-14 07:35:58 -0400

    Created an html page and used it to send an req, and res from it.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) =>{
     console.log(req.url, req.method);
     //set header content type
     res.setHeader('Content-Type', 'text/html');
+    
+    //send an html file
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello, ninjas</p>');
-    res.write('<p>hello again, ninjas</p>');
-    res.end();
+   fs.readFile('./views/index.html', (err, data) =>{
+    if(err){
+        console.log(err);
+        res.end();
+    
+        }else {
+            res.write(data);
+            res.end();
+        }
+   })
 
 });
 
